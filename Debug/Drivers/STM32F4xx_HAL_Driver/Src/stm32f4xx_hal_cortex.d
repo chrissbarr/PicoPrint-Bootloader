54c6ba5c2d2cb3422af8481ec3f6b9b71997c04b
Chris <chris@chrisbarrbuilds.com>
2017-04-30 22:44:23 +0930

    Initial Bootloader

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.d
@@ -0,0 +1,89 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Inc/stm32f4xx_hal_conf.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cm4.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Inc/stm32f4xx_hal_conf.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+D:/Git/F4Bootloader/Bootloader/F4Bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
---
+++ Src/main.c
@@ -0,0 +1,416 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include "gpio.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+USBD_HandleTypeDef USBD_Device;
+pFunction JumpToApplication;
+uint32_t JumpAddress;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+void blinkLED(int times, int duration);
+void jumpToApp();
+void boot_jump();
+uint32_t checkAndClearBootloaderFlag();
+uint32_t rtc_read_backup_reg(uint32_t BackupRegister);
+void rtc_write_backup_reg(uint32_t BackupRegister, uint32_t data);
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+	  /* STM32F446xx HAL library initialization */
+	  HAL_Init();
+
+	  /* Configure the System clock to have a frequency of 168 MHz */
+	  SystemClock_Config();
+
+	  MX_GPIO_Init();
+
+	  HAL_Delay(50);
+
+	  /* Configure user Button */
+	  blinkLED(1, 500);
+
+	  uint32_t bootloaderFlag = checkAndClearBootloaderFlag();
+
+	  if(bootloaderFlag == BOOTLOADER_FLAG_NONE) {
+
+		  /* Check if the USER Button is not pressed */
+		  if (HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin))
+		  {
+		  blinkLED(1, 500);
+
+			/* Test if user code is programmed starting from USBD_DFU_APP_DEFAULT_ADD
+			 * address */
+			if (((*(__IO uint32_t *) USBD_DFU_APP_DEFAULT_ADD) & 0xFFFD0FFF) ==
+				0x20000000)
+			{
+			  blinkLED(1, 100);
+			  HAL_Delay(10);
+			  /* Jump to user application */
+			  //JumpAddress = *(__IO uint32_t *) (USBD_DFU_APP_DEFAULT_ADD + 4);
+			  //JumpToApplication = (pFunction) JumpAddress;
+
+			  /* Initialize user application's Stack Pointer */
+			  //__set_MSP((*(__IO uint32_t *) USBD_DFU_APP_DEFAULT_ADD));
+			  //JumpToApplication();
+
+			  jumpToApp();
+			}
+		  }
+	  }
+
+	  blinkLED(5, 100);
+
+	  /* Otherwise enters DFU mode to allow user programming his application */
+	  /* Init Device Library */
+	  USBD_Init(&USBD_Device, &DFU_Desc, 0);
+
+	  /* Add Supported Class */
+	  USBD_RegisterClass(&USBD_Device, USBD_DFU_CLASS);
+
+	  /* Add DFU Media interface */
+	  USBD_DFU_RegisterMedia(&USBD_Device, &USBD_DFU_Flash_fops);
+
+	  /* Start Device Process */
+	  USBD_Start(&USBD_Device);
+
+	  /* Run Application (Interrupt mode) */
+	  while (1)
+	  {
+	  }
+
+}
+
+void blinkLED(int times, int duration) {
+	for(int i = 0; i < times; i++) {
+		HAL_GPIO_WritePin(STATUS_LED_GPIO_Port, STATUS_LED_Pin, GPIO_PIN_SET);
+		HAL_Delay(duration);
+		HAL_GPIO_WritePin(STATUS_LED_GPIO_Port, STATUS_LED_Pin, GPIO_PIN_RESET);
+		HAL_Delay(duration);
+	}
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 6;
+  RCC_OscInitStruct.PLL.PLLN = 180;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Activate the Over-Drive mode 
+    */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLRCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CLK48;
+  PeriphClkInitStruct.PLLSAI.PLLSAIM = 6;
+  PeriphClkInitStruct.PLLSAI.PLLSAIN = 96;
+  PeriphClkInitStruct.PLLSAI.PLLSAIQ = 2;
+  PeriphClkInitStruct.PLLSAI.PLLSAIP = RCC_PLLSAIP_DIV4;
+  PeriphClkInitStruct.PLLSAIDivQ = 1;
+  PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48CLKSOURCE_PLLSAIP;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USER CODE BEGIN 4 */
+void jumpToApp(void) {
+
+	//disable global interrupt
+	//__disable_irq();
+
+	// Switch off core clock before switching vector table
+	SysTick->CTRL = 0 ;
+
+	//disable all peripheral interrupts
+	HAL_NVIC_DisableIRQ(SysTick_IRQn);
+	HAL_NVIC_DisableIRQ(WWDG_IRQn);
+	HAL_NVIC_DisableIRQ(PVD_IRQn);
+	HAL_NVIC_DisableIRQ(TAMP_STAMP_IRQn);
+	HAL_NVIC_DisableIRQ(RTC_WKUP_IRQn);
+	HAL_NVIC_DisableIRQ(FLASH_IRQn);
+	HAL_NVIC_DisableIRQ(RCC_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI0_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI1_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI2_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI3_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI4_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream0_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream1_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream2_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream3_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream4_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream5_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream6_IRQn);
+	HAL_NVIC_DisableIRQ(ADC_IRQn);
+	HAL_NVIC_DisableIRQ(CAN1_TX_IRQn);
+	HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
+	HAL_NVIC_DisableIRQ(CAN1_RX1_IRQn);
+	HAL_NVIC_DisableIRQ(CAN1_SCE_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI9_5_IRQn);
+	HAL_NVIC_DisableIRQ(TIM1_BRK_TIM9_IRQn);
+	HAL_NVIC_DisableIRQ(TIM1_UP_TIM10_IRQn);
+	HAL_NVIC_DisableIRQ(TIM1_TRG_COM_TIM11_IRQn);
+	HAL_NVIC_DisableIRQ(TIM1_CC_IRQn);
+	HAL_NVIC_DisableIRQ(TIM2_IRQn);
+	HAL_NVIC_DisableIRQ(TIM3_IRQn);
+	HAL_NVIC_DisableIRQ(TIM4_IRQn);
+	HAL_NVIC_DisableIRQ(I2C1_EV_IRQn);
+	HAL_NVIC_DisableIRQ(I2C1_ER_IRQn);
+	HAL_NVIC_DisableIRQ(I2C2_EV_IRQn);
+	HAL_NVIC_DisableIRQ(I2C2_ER_IRQn);
+	HAL_NVIC_DisableIRQ(SPI1_IRQn);
+	HAL_NVIC_DisableIRQ(SPI2_IRQn);
+	HAL_NVIC_DisableIRQ(USART1_IRQn);
+	HAL_NVIC_DisableIRQ(USART2_IRQn);
+	HAL_NVIC_DisableIRQ(USART3_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI15_10_IRQn);
+	HAL_NVIC_DisableIRQ(RTC_Alarm_IRQn);
+	HAL_NVIC_DisableIRQ(OTG_FS_WKUP_IRQn);
+	HAL_NVIC_DisableIRQ(TIM8_BRK_TIM12_IRQn);
+	HAL_NVIC_DisableIRQ(TIM8_UP_TIM13_IRQn);
+	HAL_NVIC_DisableIRQ(TIM8_TRG_COM_TIM14_IRQn);
+	HAL_NVIC_DisableIRQ(TIM8_CC_IRQn);
+	HAL_NVIC_DisableIRQ(DMA1_Stream7_IRQn);
+	HAL_NVIC_DisableIRQ(FMC_IRQn);
+	HAL_NVIC_DisableIRQ(SDIO_IRQn);
+	HAL_NVIC_DisableIRQ(TIM5_IRQn);
+	HAL_NVIC_DisableIRQ(SPI3_IRQn);
+	HAL_NVIC_DisableIRQ(UART4_IRQn);
+	HAL_NVIC_DisableIRQ(UART5_IRQn);
+	HAL_NVIC_DisableIRQ(TIM6_DAC_IRQn);
+	HAL_NVIC_DisableIRQ(TIM7_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream0_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream1_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream2_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream3_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream4_IRQn);
+	HAL_NVIC_DisableIRQ(CAN2_TX_IRQn);
+	HAL_NVIC_DisableIRQ(CAN2_RX0_IRQn);
+	HAL_NVIC_DisableIRQ(CAN2_RX1_IRQn);
+	HAL_NVIC_DisableIRQ(CAN2_SCE_IRQn);
+	HAL_NVIC_DisableIRQ(OTG_FS_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream5_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream6_IRQn);
+	HAL_NVIC_DisableIRQ(DMA2_Stream7_IRQn);
+	HAL_NVIC_DisableIRQ(USART6_IRQn);
+	HAL_NVIC_DisableIRQ(I2C3_EV_IRQn);
+	HAL_NVIC_DisableIRQ(I2C3_ER_IRQn);
+	HAL_NVIC_DisableIRQ(OTG_HS_EP1_OUT_IRQn);
+	HAL_NVIC_DisableIRQ(OTG_HS_EP1_IN_IRQn);
+	HAL_NVIC_DisableIRQ(OTG_HS_WKUP_IRQn);
+	HAL_NVIC_DisableIRQ(OTG_HS_IRQn);
+	HAL_NVIC_DisableIRQ(DCMI_IRQn);
+	HAL_NVIC_DisableIRQ(FPU_IRQn);
+	HAL_NVIC_DisableIRQ(SPI4_IRQn);
+	HAL_NVIC_DisableIRQ(SAI1_IRQn);
+	HAL_NVIC_DisableIRQ(SAI2_IRQn);
+	HAL_NVIC_DisableIRQ(QUADSPI_IRQn);
+	HAL_NVIC_DisableIRQ(CEC_IRQn);
+	HAL_NVIC_DisableIRQ(SPDIF_RX_IRQn);
+	HAL_NVIC_DisableIRQ(FMPI2C1_EV_IRQn);
+	HAL_NVIC_DisableIRQ(FMPI2C1_ER_IRQn);
+
+	// Switch vector table
+	SCB->VTOR = USBD_DFU_APP_DEFAULT_ADD ;
+
+	//Jump to start address
+	boot_jump() ;
+
+}
+
+void boot_jump() {
+	/* Jump to user application */
+	JumpAddress = *(__IO uint32_t *) (USBD_DFU_APP_DEFAULT_ADD + 4);
+	JumpToApplication = (pFunction) JumpAddress;
+
+	/* Initialize user application's Stack Pointer */
+	__set_MSP((*(__IO uint32_t *) USBD_DFU_APP_DEFAULT_ADD));
+	JumpToApplication();
+}
+
+uint32_t checkAndClearBootloaderFlag() {
+	uint32_t bootloaderFlag = rtc_read_backup_reg(BOOTLOADER_FLAG_REGISTER);
+
+	rtc_write_backup_reg(BOOTLOADER_FLAG_REGISTER, BOOTLOADER_FLAG_NONE);
+
+	return bootloaderFlag;
+
+}
+
+uint32_t rtc_read_backup_reg(uint32_t BackupRegister) {
+	RTC_HandleTypeDef RtcHandle;
+	RtcHandle.Instance = RTC;
+	return HAL_RTCEx_BKUPRead(&RtcHandle, BackupRegister);
+}
+
+void rtc_write_backup_reg(uint32_t BackupRegister, uint32_t data) {
+	RTC_HandleTypeDef RtcHandle;
+	RtcHandle.Instance = RTC;
+	HAL_PWR_EnableBkUpAccess();
+	HAL_RTCEx_BKUPWrite(&RtcHandle, BackupRegister, data);
+	HAL_PWR_DisableBkUpAccess();
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
